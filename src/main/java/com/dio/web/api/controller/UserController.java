53ebb4edcd544e99b5a70546922adefe9ca907a8
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-23 11:15:53 -0300

    Added User Controller
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/dio/web/api/controller/UserController.java
+++ src/main/java/com/dio/web/api/controller/UserController.java
@@ -1,2 +1,35 @@
-package com.dio.web.api.controller;public class UserController {
+package com.dio.web.api.controller;
+
+import com.dio.web.api.model.User;
+import com.dio.web.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/fetch")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/get/{username}")
+    public User getOne(@PathVariable("username") String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping("/create")
+    public void saveUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
 }
