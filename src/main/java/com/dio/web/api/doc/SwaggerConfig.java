83a78ff544a6d70c6ad7106bbc93f5f475135c56
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-23 11:40:24 -0300

    Added Swagger configuration file
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/dio/web/api/doc/SwaggerConfig.java
+++ src/main/java/com/dio/web/api/doc/SwaggerConfig.java
@@ -1,4 +1,57 @@
-package com.dio.web.api.config;
+package com.dio.web.api.doc;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+    private Contact contact() {
+        return new Contact(
+                "Barros",
+                "https://github.com/Barros313",
+                "barros313@gmail.com"
+        );
+    }
+
+    private ApiInfoBuilder informationsApi() {
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Test - Rest API");
+        apiInfoBuilder.description("API de teste utilizando Springboot REST API");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Open Source");
+        apiInfoBuilder.license("Licença teste");
+        apiInfoBuilder.licenseUrl("http://www.testlicense.com");
+        apiInfoBuilder.contact(this.contact());
+
+        return apiInfoBuilder;
+    }
+
+    @Bean
+    public Docket detailsApi() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+
+        docket
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dio.web.api.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.informationsApi().build())
+                .consumes(new HashSet<String>(Arrays.asList("application/json")))
+                .produces(new HashSet<String>(Arrays.asList("application/json")));
+
+        return docket;
+    }
+
 }
