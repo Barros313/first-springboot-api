9547a71ea443983141ba7a4b5748d0278f58239d
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-07-23 11:13:02 -0300

    Added hello world screen
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- src/main/java/com/dio/web/api/controller/WelcomeController.java
+++ src/main/java/com/dio/web/api/controller/WelcomeController.java
@@ -1,2 +1,12 @@
-package com.dio.web.api.controller;public class WelcomeController {
+package com.dio.web.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Hello World";
+    }
 }
